6d551de8032fbccbe2e68e7d552dd8c88d6ced65
lleflour <louise.leflour@sfr.fr>
2024-03-11 17:22:50 +0100

    fonction obtenir toute mesures d'une grandeur

--- backend/src/main/java/isis/projet/backend/controller/SimpleController.java
+++ backend/src/main/java/isis/projet/backend/controller/SimpleController.java
@@ -1,11 +1,17 @@
 package isis.projet.backend.controller;
 
+import isis.projet.backend.entity.Capteur;
+import isis.projet.backend.entity.Mesure;
 import isis.projet.backend.service.CapteurService;
+import isis.projet.backend.service.MesureService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 import java.util.Map;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -13,10 +19,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Slf4j
 //ici toute les URLs de mon API que je voudrais appeler dans mon front
 public class SimpleController {
-    private CapteurService CapteurService;
+    private CapteurService capteurService;
+    private MesureService mesureService;
 
-    public SimpleController(CapteurService CapteurService) {
-        this.CapteurService = CapteurService;
+    public SimpleController(CapteurService capteurService) {
+        this.capteurService = capteurService;
     }
 
     @GetMapping("/hello")
@@ -25,10 +32,11 @@ public class SimpleController {
         return Map.of("message", "Hello, World !");
     }
 
-    @GetMapping("/capteurs")
-    public Map<Integer,String> getCapteurs() {
-        log.info("test");
-        return Map.of(1,CapteurService.quelType());
+    @GetMapping("/mesures/{capteurs}")
+    public ResponseEntity<List<Mesure>> getMesuresParGrandeur(@PathVariable(name = "capteur") final Capteur capteur) {
+        log.info("Obtenir toute les mesures d'une grandeur");
+        final List<Mesure> mesures =  mesureService.getMesuresParGrandeur(capteur);
+        return new ResponseEntity<>(mesures, HttpStatus.OK);
     }
     
 
--- backend/src/main/java/isis/projet/backend/service/MesureService.java
+++ backend/src/main/java/isis/projet/backend/service/MesureService.java
@@ -1,16 +1,26 @@
 package isis.projet.backend.service;
 
 import isis.projet.backend.dao.MesureRepository;
+import isis.projet.backend.entity.Capteur;
+import isis.projet.backend.entity.Mesure;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MesureService {
+
+    @Autowired
     private final MesureRepository mesureDao;
 
     public MesureService(MesureRepository mesureDao) {
         this.mesureDao = mesureDao;
     }
 
-
+    public List<Mesure> getMesuresParGrandeur(final Capteur cap){
+        return mesureDao.trouverMesuresParCapteur(cap);
+    }
     
 }
